34ed1f10ae8dd1d38e0c8c9ba801c8dea2506065
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-07-21 11:11:56 +0900

    [Gold I] Title: 수열과 쿼리 17, Time: 76 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Gold/14438. 수열과 쿼리 17/수열과 쿼리 17.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int INF = 0x7FFFFFFF;
+
+int tree[100'000 * 4 + 16];
+
+void update(int left, int right, int idx, int cidx, int cval) {
+	if (cidx < left || right < cidx) {
+		return;
+	}
+	if (left == right) {
+		tree[idx] = cval;
+		return;
+	}
+	int mid = (left + right) / 2;
+	update(left, mid, idx * 2, cidx, cval);
+	update(mid+1, right, idx * 2 + 1, cidx, cval);
+	tree[idx] = min(tree[idx * 2], tree[idx * 2 + 1]);
+	return;
+}
+
+int query(int left, int right, int idx, int st, int ed) {
+	if (ed < left || right < st) {
+		return INF;
+	}
+	if (st <= left && right <= ed) {
+		return tree[idx];
+	}
+	int mid = (left + right) / 2;
+	return min(query(left, mid, idx * 2, st, ed), query(mid + 1, right, idx * 2 + 1, st, ed));
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, m;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int x;
+		cin >> x;
+		update(1, n, 1, i, x);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int command, x, y;
+		cin >> command >> x >> y;
+		if (command == 1) {
+			update(1, n, 1, x, y);
+		}
+		else {
+			cout << query(1, n, 1, x, y) << "\n";
+		}
+	}
+	return 0;
+}

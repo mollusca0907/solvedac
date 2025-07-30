5768cf5bb05c9635cb9d5dba233d2d22dfedb99e
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-07-31 00:02:42 +0900

    [Gold II] Title: 좋아하는 다이아몬드가 안경을 깜빡했다, Time: 188 ms, Memory: 25696 KB -BaekjoonHub

--- 백준/Gold/34079. 좋아하는 다이아몬드가 안경을 깜빡했다/좋아하는 다이아몬드가 안경을 깜빡했다.cc
+++ 백준/Gold/34079. 좋아하는 다이아몬드가 안경을 깜빡했다/좋아하는 다이아몬드가 안경을 깜빡했다.cc
@@ -1,78 +1,61 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <queue>
 using namespace std;
-using ll = long long;
 
-const ll MOD1 = 1'000'000'007;
-const ll MOD2 = 1'000'000'009;
+const int MAX = 200005;
+int n, m;
+vector<int> graph[MAX];
+int dist1[MAX], dist2[MAX];
+bool good[MAX];
+vector<int> depth[MAX];
 
-ll n, m;
-vector<ll> graph[200'007];
-vector<ll> cnt[2][2];
-vector<ll> dist[2];
-
-void bfs(ll start) {
-    ll idx = (start == n) ? 1 : 0;
-
-    dist[idx].assign(n + 1, -1);
-    cnt[idx][0].assign(n + 1, 0);
-    cnt[idx][1].assign(n + 1, 0);
-
-    queue<ll> q;
-    dist[idx][start] = 0;
-    cnt[idx][0][start] = 1;
-    cnt[idx][1][start] = 1;
+void bfs(int start, int* dist) {
+    fill(dist, dist + n + 1, -1);
+    queue<int> q;
+    dist[start] = 0;
     q.push(start);
-
     while (!q.empty()) {
-        ll cur = q.front(); q.pop();
-        for (ll next : graph[cur]) {
-            if (dist[idx][next] == -1) {
-                dist[idx][next] = dist[idx][cur] + 1;
-                cnt[idx][0][next] = cnt[idx][0][cur];
-                cnt[idx][1][next] = cnt[idx][1][cur];
+        int cur = q.front(); q.pop();
+        for (int next : graph[cur]) {
+            if (dist[next] == -1) {
+                dist[next] = dist[cur] + 1;
                 q.push(next);
-            }
-            else if (dist[idx][next] == dist[idx][cur] + 1) {
-                cnt[idx][0][next] = (cnt[idx][0][next] + cnt[idx][0][cur]) % MOD1;
-                cnt[idx][1][next] = (cnt[idx][1][next] + cnt[idx][1][cur]) % MOD2;
             }
         }
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     cin >> n >> m;
-
-    for (ll i = 0; i < m; i++) {
-        ll u, v;
+    while (m--) {
+        int u, v;
         cin >> u >> v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
 
-    bfs(1);
-    bfs(n);
+    bfs(1, dist1);
+    bfs(n, dist2);
+    int total = dist1[n];
 
-    ll ans = 1;
-    for (ll i = 2; i < n; i++) {
-        if (dist[0][i] + dist[1][i] == dist[0][n]) {
-            bool check1 = (cnt[0][0][i] * cnt[1][0][i]) % MOD1 == cnt[0][0][n];
-            bool check2 = (cnt[0][1][i] * cnt[1][1][i]) % MOD2 == cnt[0][1][n];
-
-            if (check1 && check2) {
-                ans = i;
-                break;
-            }
+    for (int i = 2; i < n; ++i) {
+        if (dist1[i] + dist2[i] == total) {
+            good[i] = true;
+            depth[dist1[i]].push_back(i);
         }
     }
 
-    cout << ans;
+    for (int d = 1; d < total; ++d) {
+        if (depth[d].size() == 1) {
+            cout << depth[d][0];
+            return 0;
+        }
+    }
 
+    cout << 1;
     return 0;
 }

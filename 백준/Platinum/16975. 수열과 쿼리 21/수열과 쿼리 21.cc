cc5fb441a1ec2a509ad9921eb688770787278b59
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-08-15 03:40:36 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 96 ms, Memory: 64520 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+
+ll tree[1'000'000 * 4 + 16];
+ll lazy[1'000'000 * 4 + 16];
+
+void push(ll idx, ll left, ll right) {
+	if (lazy[idx] != 0) {
+		tree[idx] += (right - left + 1) * lazy[idx];
+		if (left != right) { //not leaf then
+			lazy[idx * 2] += lazy[idx];
+			lazy[idx * 2 + 1] += lazy[idx];
+		}
+		lazy[idx] = 0;
+	}
+}
+
+
+void update(ll idx, ll left, ll right, ll cleft, ll cright, ll val) {
+	push(idx, left, right);
+	if (right < cleft || cright < left) {
+		return;
+	}
+	if (cleft <= left && right <= cright) {
+		lazy[idx] += val;
+		push(idx, left, right);
+		return;
+	}
+
+	ll mid = (left + right) / 2;
+	update(idx * 2, left, mid, cleft, cright, val);
+	update(idx * 2 + 1, mid + 1, right, cleft, cright, val);
+	tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+}
+
+ll query(ll idx, ll left, ll right, ll qleft, ll qright) {
+	push(idx, left, right);
+	if (right < qleft || qright < left) {
+		return 0;
+	}
+	if (qleft <= left && right <= qright) {
+		return tree[idx];
+	}
+	ll mid = (left + right) / 2;
+	return query(idx * 2, left, mid, qleft, qright) + query(idx * 2 + 1, mid + 1, right, qleft, qright);
+}
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+
+	ll n, m;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		ll x;
+		cin >> x;
+		update(1, 1, n, i, i, x);
+	}
+
+	cin >> m;
+
+	for (int i = 1; i <= m; i++) {
+		ll command;
+		cin >> command;
+		if (command == 1) {//update
+			ll a, b, c;
+			cin >> a >> b >> c;
+			update(1, 1, n, a, b, c);
+		}
+		else {//query
+			ll a;
+			cin >> a;
+			cout << 1LL * query(1, 1, n, a, a) << "\n";
+		}
+	}
+
+
+	return 0;
+}

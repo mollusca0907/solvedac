11ffd3369a7d04357b92978b54f24e0b84cbeb18
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-08-18 14:52:42 +0900

    [Platinum V] Title: 찾기, Time: 108 ms, Memory: 12656 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -4,73 +4,83 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> getpi(string& pattern) {
+vector<int> getpi(string pattern) {
 
-	int size = pattern.size();
+	int size = pattern.length();
 	vector<int> pi(size, 0);
+	int start = 1;
+	int matched_len = 0;
 
-	int begin = 1;
-	int matched_cnt = 0;
-
-	while (begin + matched_cnt < size) {
-		if (pattern[begin + matched_cnt] == pattern[matched_cnt]) {
-			matched_cnt++;
-			pi[begin + matched_cnt - 1] = matched_cnt;
+	while (start + matched_len < size) {
+		//원본 문자열 대비 복사된 문자열은 start만큼의 gap을 가지고 오른쪽으로 이동
+		//이 gap을 메우기 위해 원본 문자열에서는 start + matched 접근, 복사된 문자열은 matched 접근
+		if (pattern[start + matched_len] == pattern[matched_len]) { //둘이 같으면?????????? 
+			matched_len++; //길이 증가
+			pi[start + matched_len - 1] = matched_len; //pi배열에 넣음 (0-based)
 		}
-		else {
-			if (matched_cnt == 0) {
-				begin++;
+		else { //다르면???????????
+			if (matched_len == 0) { //다른데 맞는것도 없었으면???????????
+				start++; //무지성 +1해버리기
 			}
-			else {
-				begin += matched_cnt - pi[matched_cnt - 1];
-				matched_cnt = pi[matched_cnt - 1];
+			else { //적어도 뭐가 맞긴 하면????????????
+				start += matched_len - pi[matched_len - 1]; //최대 맞은 개수에서 최장 접두사 길이만큼 빼서 점프
+				matched_len = pi[matched_len - 1]; //점프 후 맞은 개수는 최장 접미사 길이와 같다
 			}
 		}
-
 	}
+
 	return pi;
 }
 
-vector<int> kmp(string& pattern, string& s) {
+vector<int> kmp(string pattern, string s) {
 	vector<int> ans;
-
 	int p_size = pattern.size();
 	int s_size = s.size();
-	auto pi = getpi(pattern);
 
-	int begin = 0;
-	int matched_cnt = 0;
+	vector<int> pi = getpi(pattern);
 
-	while (begin <= s_size - p_size) {
-		if (matched_cnt < p_size && s[begin + matched_cnt] == pattern[matched_cnt]) {
-			matched_cnt++;
-			if (matched_cnt == p_size) {
-				ans.push_back(begin);
+	int start = 0;
+	int matched_len = 0;
+
+	while (start <= s_size - p_size) {
+		if (matched_len < p_size && s[start + matched_len] == pattern[matched_len]) {
+			matched_len++;
+			if (matched_len == p_size) {
+				ans.push_back(start);
 			}
 		}
 		else {
-			if (matched_cnt == 0) {
-				begin++;
+			if (matched_len == 0) {
+				start++;
 			}
 			else {
-				begin += matched_cnt - pi[matched_cnt - 1];
-				matched_cnt = pi[matched_cnt - 1];
+				start += matched_len - pi[matched_len - 1];
+				matched_len = pi[matched_len - 1];
 			}
 		}
 	}
 	return ans;
 }
 
+
 int main() {
 
-	string s, pattern;
+
+
+	string s;
+	string pattern;
 	getline(cin, s);
 	getline(cin, pattern);
 
-	auto matched = kmp(pattern, s);
-	cout << matched.size() << "\n";
-	for (auto i : matched) {
-		cout << i+1 << " ";
+	auto ans = kmp(pattern, s);
+
+	int len = ans.size();
+	cout << len << "\n";
+	for (int i = 0; i < len; i++) {
+		cout << ans[i]+1 << " ";
 	}
+
+
+
 	return 0;
 }

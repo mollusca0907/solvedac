e675e7edd520b1bc05fdea1e46ce0a5a7a1dff38
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-08-19 14:00:41 +0900

    [Gold V] Title: 벼락치기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.cc
+++ 백준/Gold/14728. 벼락치기/벼락치기.cc
@@ -2,33 +2,32 @@
 #include <algorithm>
 using namespace std;
 
-int w[101]; //비용
-int v[101]; //가치
+int w[101];
+int v[101];
 
-int dp[101][10001];
-// 단원수, 총 시간
+int dp[10001];
+
 int n, k;
+
 
 int main() {
 
 	cin >> n >> k;
-
 	for (int i = 1; i <= n; i++) {
 		cin >> w[i] >> v[i];
 	}
 
-
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
+		for (int j = k; j >= 1; j--) {
 			if (j >= w[i]) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-			}
-			else {
-				dp[i][j] = dp[i - 1][j];
+				dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
 			}
 		}
 	}
 
-	cout << dp[n][k];
+
+	cout << dp[k];
+
+
 	return 0;
 }

dbd14dbd47ffd9592a417f73f879a461e9a605ee
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-08-20 19:39:44 +0900

    [Bronze III] Title: 공 넣기, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,8 @@
+n,m =map(int,input().split())
+b=[0 for _ in range(n + 1)]
+for _ in range(m):
+    i,j,k=map(int, input().split())
+    for l in range(i, j + 1):
+        b[l] = k
+for i in range(1, n + 1):
+    print(b[i], end=' ')

ad77591b3c3a5fa3047487632b7261408a1eea96
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-09-09 00:15:50 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSharpTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+
+            Console.WriteLine(a+b);
+
+        }
+    }
+}

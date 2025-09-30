86d1f1085019b83eeb3f1f622a3ecc1733fe5205
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-09-30 23:33:19 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int w1, h1, w2, h2;
+    cin >> w1 >> h1 >> w2 >> h2;
+    if(w1 - w2 >= 2 && h1 - h2 >= 2){
+        cout << 1;  
+    } 
+    else{
+        cout << 0;  
+    } 
+    return 0;
+}

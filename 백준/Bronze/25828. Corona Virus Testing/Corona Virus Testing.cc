ad8e76c3e20dd9c2196306050f3b7187dc8cb23d
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-09-30 23:42:57 +0900

    [Bronze IV] Title: Corona Virus Testing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25828. Corona Virus Testing/Corona Virus Testing.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int g, p, t;
+	cin >> g >> p >> t;
+
+	int group = g + p * t;
+	int one = g * p;
+
+	if (one < group) {
+		cout << 1;
+	}
+	else if (one > group) {
+		cout << 2;
+	}
+	else {
+		cout << 0;
+	}
+
+	return 0;
+}

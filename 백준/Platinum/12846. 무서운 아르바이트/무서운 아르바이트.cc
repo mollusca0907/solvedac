9b44dbfe9ad1a4ed6d608e8c8e808bcd9fb5d303
mollusca <116443285+mollusca0907@users.noreply.github.com>
2025-10-29 00:33:16 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 40 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int n;
+
+vector<ll> min_tree(100'000 * 4 + 16);
+
+vector<ll> arr(100'000 + 16);
+
+void min_update(ll idx, ll left, ll right, ll cidx) {
+	if (right < cidx || cidx < left) {
+		return;
+	}
+	if (left == right) {
+		min_tree[idx] = cidx;
+		return;
+	}
+
+	ll mid = (left + right) / 2;
+	min_update(idx * 2, left, mid, cidx);
+	min_update(idx * 2 + 1, mid + 1, right, cidx);
+
+	min_tree[idx] = (arr[min_tree[idx * 2]] < arr[min_tree[idx * 2 + 1]]) ? min_tree[idx * 2] : min_tree[idx * 2 + 1];
+	return;
+}
+
+ll min_query(ll idx, ll left, ll right, ll st, ll ed) {
+	if (right < st || ed < left) {
+		return -1;
+	}
+	if (st <= left && right <= ed) {
+		return min_tree[idx];
+	}
+	ll mid = (left + right) / 2;
+	ll lc = min_query(idx * 2, left, mid, st, ed);
+	ll rc = min_query(idx * 2 + 1, mid + 1, right, st, ed);
+	if (lc == -1) {
+		return rc;
+	}
+	if (rc == -1) {
+		return lc;
+	}
+	return (arr[lc] < arr[rc]) ? lc : rc;
+}
+
+ll solve(ll left, ll right) {
+
+	if (right < left) {
+		return 0;
+	}
+
+	ll min_idx = min_query(1, 1, n, left, right);
+	ll s = arr[min_idx] * (right - left + 1);
+	ll lc = solve(left, min_idx - 1);
+	ll rc = solve(min_idx + 1, right);
+	return max({ s, lc, rc });
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	min_tree.assign(100'000 * 4 + 16, 0);
+	arr.assign(100'000 + 16, 0);
+
+	cin >> n;
+	if (n == 0) {
+		return 0;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		ll x;
+		cin >> x;
+		arr[i] = x;
+		min_update(1, 1, n, i);
+	}
+
+	cout << 1LL * solve(1, n) << "\n";
+
+	return 0;
+}
